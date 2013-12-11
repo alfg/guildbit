bb7ab067906190120e0e234c002608244e8e394e
Alf <alf.g.jr@gmail.com>
2013-12-11 00:00:39 -0800

    Updated duration values.

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,6 @@ class DeployServerForm(Form):
     duration = SelectField('duration',
                            validators=[DataRequired()],
                            choices=[
-                               ('0', 'Set a Duration'),
                                ('4', '4 Hours'),
                                ('8', '8 Hours'),
                                ('16', '16 Hours'),
