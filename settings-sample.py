a8e68f391a3300ed759b81870ff8c7201c591b87
Alf <alf.g.jr@gmail.com>
2014-03-03 23:22:28 -0800

    Update settings-sample.py with more defaults.

--- settings-sample.py
+++ settings-sample.py
@@ -1,7 +1,11 @@
+import os
+
 APP_HOST = '0.0.0.0'
 APP_PORT = 4000
 APP_DEBUG = True
-APP_SESSION_KEY = 'testsession'
+APP_SESSION_KEY = 'super-secret'
+CSRF_ENABLED = True
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE_URI = 'sqlite:////tmp/test.db'
 BROKER_URL = 'redis://localhost:6379/0'
@@ -10,3 +14,27 @@ MURMUR_REST_HOST = 'http://localhost:5000'
 
 DEFAULT_MAX_USERS = 10
 DEFAULT_CHANNEL_NAME = 'GuildBit.com Mumble Server'
+
+MAIL_SERVER = 'email-smtp.us-east-1.amazonaws.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = True
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+DEFAULT_MAIL_SENDER = 'alf.g.jr@gmail.com'
+EMAIL_RECIPIENTS = ['alf.g.jr@gmail.com']
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'}]
+
+MURMUR_HOSTS = [
+    {
+        'name': 'GuildBit',
+        'address': 'localhost:5000',
+        'uri': 'http://localhost:5000',
+        'contact': 'alf.g.jr@gmail.com',
+        'status': 'active',
+        'capacity': 100,
+    }
+]
